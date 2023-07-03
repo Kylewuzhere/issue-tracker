7beda18ffd5a24892dafeca9d0e8b6ca287f633c
kyle vasquez <thatepicdude1026@gmail.com>
2023-07-03 20:48:07 +1200

    update: login callback url

--- issue-tracker/src/components/LoginCard.js
+++ issue-tracker/src/components/LoginCard.js
@@ -4,13 +4,13 @@ import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function LoginCard() {
-  const [displayCredentials, setDisplayCredentials] = useState(false)
+  const [displayCredentials, setDisplayCredentials] = useState(false);
 
   const handleClick = () => {
     setDisplayCredentials((prevState) => {
-      return !prevState
-    })
-  }
+      return !prevState;
+    });
+  };
   return (
     <div className="bg-white border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 space-y-6">
       <h3 className="text-black text-2xl font-bold text-center">
@@ -21,21 +21,21 @@ export default function LoginCard() {
         onClick={() =>
           signIn("auth0", {
             redirect: true,
-            callbackUrl: "/dashboard",
+            callbackUrl: "/home",
           })
         }
       >
         Log in with Auth0
       </button>
-      <button
-        className="text-black"
-        onClick={handleClick}>Click here to show demo user credentials</button>
-      {displayCredentials &&
+      <button className="text-black" onClick={handleClick}>
+        Click here to show demo user credentials
+      </button>
+      {displayCredentials && (
         <div className="text-black">
           <p>Email: issuetrackerdemo@test.com</p>
           <p>Password: 3fh&ZBDGyQDvKBd</p>
         </div>
-      }
+      )}
     </div>
   );
 }
