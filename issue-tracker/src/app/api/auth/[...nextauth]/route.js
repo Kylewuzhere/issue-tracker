d9614a2db82f8a419f03e5758ed0724ae9765051
kyle vasquez <thatepicdude1026@gmail.com>
2023-07-03 20:48:30 +1200

    prettier changes

--- issue-tracker/src/app/api/auth/[...nextauth]/route.js
+++ issue-tracker/src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ export const authOptions = {
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       issuer: process.env.AUTH0_ISSUER,
-      authorization: { params: { prompt: 'login' } },
+      authorization: { params: { prompt: "login" } },
     }),
   ],
 };
